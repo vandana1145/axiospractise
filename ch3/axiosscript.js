9fdfc58d5697de9d37016f67aca7909f5fd959cc
Vandana Kumari <vandana.kumari@semapp.de>
2021-03-26 13:35:49 +0100

    fetching, rendering, error handling of multiple data using async await

--- ch3/axiosscript.js
+++ ch3/axiosscript.js
@@ -60,23 +60,91 @@ document.getElementById("btn").addEventListener("click", makerequest);
 // }
 
 // API Multiple data rendering in browser | Promise then
-function makerequest(){
-    console.log("Button Clicked")
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-    .then((res) => {
+// function makerequest(){
+//     console.log("Button Clicked")
+//     axios.get('https://jsonplaceholder.typicode.com/posts')
+//     .then((res) => {
+//         console.log(res)
+//         console.log("Data:", res.data)
+//         let output = document.getElementById("alldata")
+//         res.data.forEach(element => {
+//             output.innerHTML += `
+//             <div>UserId: ${element.userId}</div>
+//             <div>Id: ${element.id}</div>
+//             <div>Title: ${element.title}</div>
+//             <div>Body: ${element.body}</div> <hr>
+//             `
+//         });
+//     })
+//     .catch((error) => {
+//         console.log(error)
+//     })
+// }
+
+
+// async await | single data fetch from api
+// async function makerequest(){
+//     try {
+//         console.log("Button Clicked!")
+//         const res = await axios('https://jsonplaceholder.typicode.com/posts/1')
+//         console.log(res)
+//         console.log("Data:", res.data)
+//         console.log("User Id:", res.data.userId)
+//         console.log("Id:", res.data.id)
+//         console.log("Title:", res.data.title)
+//         console.log("Body:", res.data.body)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// async await | single data rendering in browser
+// async function makerequest(){
+//     try {
+//         console.log("Button Clicked!")
+//         const res = await axios.get('https://jsonplaceholder.typicode.com/posts/1')
+//         console.log(res)
+//         document.getElementById("div1").innerHTML = res.data.id 
+//         document.getElementById("div2").innerHTML = res.data.title
+//         document.getElementById("div3").innerHTML = res.data.body
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// async await | multiple data fetch from api
+// async function makerequest(){
+//     try {
+//         console.log("Button Clicked!")
+//         const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+//         console.log(res)
+//         res.data.forEach(element => {
+//             console.log("Id:", element.id)
+//             console.log("Title:", element.title)
+//             console.log("Body:", element.body)
+//         });
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// async await | multiple data rendering in browser
+async function makerequest(){
+    try {
+        console.log("Button Clicked!")
+        const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
         console.log(res)
-        console.log("Data:", res.data)
+        console.log(res.data)
         let output = document.getElementById("alldata")
         res.data.forEach(element => {
+            //appending the element objects to the output div
             output.innerHTML += `
-            <div>UserId: ${element.userId}</div>
             <div>Id: ${element.id}</div>
             <div>Title: ${element.title}</div>
-            <div>Body: ${element.body}</div> <hr>
+            <div>Body: ${element.body}</div><hr>
             `
         });
-    })
-    .catch((error) => {
+    } catch (error) {
         console.log(error)
-    })
+    }
 }
